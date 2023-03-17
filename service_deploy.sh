e52ab99133a9e3e90c582a1a14106210f950139a
night <night_forget@126.com>
2023-03-17 16:43:53 +0800

    降低配置

--- service_deploy.sh
+++ service_deploy.sh
@@ -7,7 +7,7 @@ then
   sleep 5
 fi
 
-sudo nohup java -jar -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=512m -Xms5120m -Xmx5120m -Xmn5120m -Xss256k -XX:SurvivorRatio=8 -XX:+UseG1GC -Dspring.profiles.active=prod /home/ecs-user/app/maxi.jar  >> /home/ecs-user/logs/appdef.log 2>&1   &
+sudo nohup java -jar -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -Xms2048m -Xmx2048m -Xmn2048m -Xss256k -XX:SurvivorRatio=8 -XX:+UseG1GC -Dspring.profiles.active=prod /home/ecs-user/app/maxi.jar  >> /home/ecs-user/logs/appdef.log 2>&1   &
 
 sleep 10
 curl  127.0.0.1:8088/health
