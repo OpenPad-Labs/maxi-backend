5e9744c2059ed8da8f9ffd71162b8e6a50ccfbaf
night <night_forget@126.com>
2023-04-12 16:11:59 +0300

    change dir

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ git pull
 
 mvn -Dmaven.test.skip=true  package
 
-cp /home/ecs-user/github/maxi-backend/target/maxi.jar /home/ecs-user/app/
+cp /home/ubuntu/github/maxi-backend/target/maxi.jar /home/ubuntu/app/
--- service_deploy.sh
+++ service_deploy.sh
@@ -1,13 +1,15 @@
 # sudo chmod a+x /home/admin/app/jnljs.jar
 
-if [ -f "/home/ecs-user/github/maxi-backend/app.pid" ];
+cd /home/ubuntu/app
+
+if [ -f "/home/ubuntu/github/maxi-backend/app.pid" ];
 then
-  cat /home/ecs-user/github/maxi-backend/app.pid |sudo xargs  kill -9
+  cat /home/ubuntu/github/maxi-backend/app.pid |sudo xargs  kill -9
   # 暂停15秒再重启,让内存回收下
   sleep 5
 fi
 
-sudo nohup java -jar -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -Xms2048m -Xmx2048m -Xmn2048m -Xss256k -XX:SurvivorRatio=8 -XX:+UseG1GC -Dspring.profiles.active=prod /home/ecs-user/app/maxi.jar  >> /home/ecs-user/logs/appdef.log 2>&1   &
+sudo nohup java -jar -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -Xms2048m -Xmx2048m -Xmn2048m -Xss256k -XX:SurvivorRatio=8 -XX:+UseG1GC -Dspring.profiles.active=prod /home/ubuntu/app/maxi.jar  >> /home/ubuntu/logs/appdef.log 2>&1   &
 
 sleep 20
 curl  127.0.0.1:8088/health
